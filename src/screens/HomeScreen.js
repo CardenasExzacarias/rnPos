9c89322109ba3b1059128fc9e9de5ce3d68403e0
CardenasExzacarias <cardenas.exzacarias@gmail.com>
2024-12-08 14:44:43 -0800

    Registrar productos
    Se comenzó a crear la pantalla para registrar los productos

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,7 +15,6 @@ const HomeScreen = ({ navigation }) => {
                 text={"Inventario"}
             />
         </View>
-
     );
 };
 
--- src/screens/inventory/RegisterProductScreen.js
+++ src/screens/inventory/RegisterProductScreen.js
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { globalStyles } from '../../styles/global';
+import { useState } from 'react';
 
 const RegisterProductScreen = () => {
+    const [name, setName] = useState('');
+    const [unitCost, setUnitCost] = useState('');
+    const [unitPrice, setUnitPrice] = useState('');
+
     return (
         <View style={globalStyles.container}>
-            <Text>Holo</Text>
+            <TextInput
+                placeholder='Nombre'
+                value={name}
+                onChange={setName}
+            />
+            <TextInput
+                placeholder='Código'
+                value={name}
+                onChange={setName}
+            />
+            <TextInput
+                placeholder='Precio de venta'
+                value={unitPrice}
+                onChange={setUnitPrice}
+            />
+            <TextInput
+                placeholder='Costo de compra'
+                value={unitCost}
+                onChange={setUnitCost}
+            />
         </View>
-    )
+    );
 };
 
 export default RegisterProductScreen;
